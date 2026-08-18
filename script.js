9f842ddfec133ed3ab77e076c70e7179dc041405
Leeeee :) <leticia.kowaleski.alves@escola.pr.gov.br>
2026-08-18 23:23:18 +0000

    atualizacao do dia 18 de agosto

---
+++ script.js
@@ -0,0 +1,26 @@
+const botaoLike = document.querySelector (".like");
+const botaoDeslike = document.querySelector (".deslike")
+
+botaoLike.addEventListener ("click", clickLike);
+botaoDeslike.addEventListener ("click", clickDeslike);
+
+
+function clickLike () {
+
+    console.log ("fui clicado");
+    let texto1=botaoLike.querySelector ("span");
+    texto1.textContent++;
+
+}
+
+function clickDeslike() {
+
+    console.log ("fui clicado");
+    let texto2=botaoDeslike.querySelector ("span");
+    texto2.textContent++;
+
+
+
+
+
+}
